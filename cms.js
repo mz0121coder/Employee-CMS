c2db036075989f951390fb047172316aca495115
Mohammad Zafar <sz7861@outlook.com>
2021-08-28 18:39:03 +0100

    Update file, fix typo

--- cms.js
+++ cms.js
@@ -1,11 +1,11 @@
 // Requirements
-const mysql = require("mysql");
+const mysql2 = require("mysql2");
 const inquirer = require("inquirer");
 const ct = require("console.table");
 
-// Create mysql connection
+// Create mysql2 connection
 const password = process.argv[2];
-const connection = mysql.createConnection({
+const connection = mysql2.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
